5f97bb3d4c082adb2fcedf4bd4bcdbbaedf6e6d0
DoReVo <frz565@gmail.com>
2023-11-20 23:42:25 +0800

    initial

---
+++ api/src/index.ts
@@ -0,0 +1,42 @@
+import { Hono } from "hono";
+import { cors } from "hono/cors";
+import { OPTIONS } from "./constant/constant";
+import { fetchTime } from "./lib/jakim";
+
+type Bindings = {
+  KronosKV: KVNamespace;
+};
+
+const server = new Hono<{ Bindings: Bindings }>();
+
+// CORS middlware
+server.use(
+  "/*",
+  cors({
+    origin: "*",
+  })
+);
+server.get("/", (c) => {
+  return c.json({ message: "Welcome to Kronos API" });
+});
+
+server.get("/selectionoptions", async (c) => {
+  return c.json(OPTIONS);
+});
+
+server.get("/time", async (c) => {
+  const { date, zone } = c.req.query();
+
+  const KV = c.env.KronosKV;
+
+  const response = await fetchTime(zone, date, KV);
+  return c.json(response);
+});
+
+server.get("/test", async (c) => {
+  const kvs = await c.env.KronosKV.list();
+  console.log(kvs);
+  return c.json({ message: "ok" });
+});
+
+export default server;
