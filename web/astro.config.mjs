efdeec6bde93390e12e393f26f7231a0de68916b
DoReVo <frz565@gmail.com>
2024-06-30 16:09:47 +0800

    add react

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -1,11 +1,15 @@
 import { defineConfig } from "astro/config";
 import UnoCSS from "unocss/astro";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     UnoCSS({
       injectReset: true,
     }),
+    react(),
   ],
 });
+
