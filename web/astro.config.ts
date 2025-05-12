4c4edecdc2e60d1ceae9c193bbd4444022fa29c6
DoReVo <frz565@gmail.com>
2025-05-12 23:29:41 +0800

    feat: add pwa integration

--- web/astro.config.ts
+++ web/astro.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "astro/config";
 import UnoCSS from "unocss/astro";
+import AstroPWA from "@vite-pwa/astro";
 
 import react from "@astrojs/react";
 
@@ -11,5 +12,13 @@ export default defineConfig({
       injectReset: true,
     }),
     react(),
+    AstroPWA({
+      registerType: "autoUpdate",
+      manifest: {
+        display: "standalone",
+        name: "Kronos Waktu Solat",
+        short_name: "Waktu Solat",
+      },
+    }),
   ],
 });
